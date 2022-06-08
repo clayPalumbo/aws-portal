187e9e2ef17d950ee83a0aea958ab1429310c5fd
clayPalumbo <clayptv@gmail.com>
2022-06-08 19:35:26 -0400

    add news

--- lib/functions/crypto-news/services/crypto-news.service.ts
+++ lib/functions/crypto-news/services/crypto-news.service.ts
@@ -2,18 +2,16 @@ import fetch from "node-fetch";
 
 export class CryptoNewsService {
   newsItems: number;
+  currencies: string;
   constructor(event: any) {
     this.newsItems = event.queryStringParameters.newsItems;
+    this.currencies = event.queryStringParameters.currencies
   }
 
   public getNews = async () => {
-    const url = "https://crypto-news-live3.p.rapidapi.com/news";
+    const url = `https://cryptopanic.com/api/v1/posts/?auth_token=${process.env.CRYPTO_PANIC_TOKEN}&currencies=${this.currencies}`;
     const options = {
       method: "GET",
-      headers: {
-        "X-RapidAPI-Host": "crypto-news-live3.p.rapidapi.com",
-        "X-RapidAPI-Key": process.env.RAPID_API_KEY as string,
-      },
     };
     const response = await fetch(url, options).then((res) => res.json());
 
